d671d735f126067249c0b17e68776a3ea2eb306f
wadiehabib <wadie4jesus@gmail.com>
2022-01-09 01:54:56 +0000

    raphael: use vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/wadiehabib/hardware_qcom-caf_sm8150_display.git hardware/qcom-caf/sm8150/display --branch=12_soviet
+rm -rf prebuilts/clang/host/linux-x86/clang-azure && git clone https://gitlab.com/Panchajanya1999/azure-clang.git prebuilts/clang/host/linux-x86/clang-azure --branch=main --depth=1
