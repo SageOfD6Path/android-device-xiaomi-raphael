bfb7a3bc47e10d4cc4fe383ab8222577b08bbf96
wadiehabib <wadie4jesus@gmail.com>
2022-02-05 14:07:26 +0200

    ancient: use dependencies instead of script

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/wadiehabib/hardware_qcom-caf_sm8150_display.git hardware/qcom-caf/sm8150/display --branch=12_soviet
-git clone https://gitlab.com/Panchajanya1999/azure-clang.git prebuilts/clang/host/linux-x86/clang-azure --branch=main --depth=1
-git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
