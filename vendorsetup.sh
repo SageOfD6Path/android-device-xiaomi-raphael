ad44f18519d4ac6cfd2cdd1b03d745e6a29ed475
SageOfD6Path <mail2anirban95@gmail.com>
2022-10-17 06:42:39 +0000

    Clone Miuicamera source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/PptO07/hardware_qcom_display.git -b 12-sm8150 hardware/qcom-caf/sm8150/display --depth=1
 git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 12l vendor/xiaomi/raphael --depth=1
 git clone https://github.com/VoltageOS-Devices/kernel_xiaomi_raphael.git -b 12l kernel/xiaomi/raphael --depth=1
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b 12.1  vendor/xiaomi/miuicamera 
