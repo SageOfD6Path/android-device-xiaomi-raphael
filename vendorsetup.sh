274aa06cb73ccb66bdf3e93a5074a5f404054cae
SageOfD6Path <mail2anirban95@gmail.com>
2022-10-29 07:50:51 +0000

    raphael: Build BSG Gcam
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,5 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/PptO07/hardware_qcom_display.git -b 12-sm8150 hardware/qcom-caf/sm8150/display --depth=1
 git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 12l vendor/xiaomi/raphael --depth=1
 git clone https://github.com/VoltageOS-Devices/kernel_xiaomi_raphael.git -b 12l kernel/xiaomi/raphael --depth=1
+git clone https://gitlab.com/SageOfD6Path/android_vendor_googlecamera.git -b master vendor/GoogleCamera
+
